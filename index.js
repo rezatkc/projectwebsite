1eec5ec6eebf5d888c4aade72057b6819df0b75e
andreepratama27 <andreepratama27@gmail.com>
2016-05-09 21:03:42 +0700

    Tambah halaman untuk testing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express  = require('express'),
 	app 	 = express(),
 	nunjucks = require('nunjucks'),
 	path 	 = require('path'),
+	bodyParser = require('body-parser'),
 	router	 = require('./router');
 
 nunjucks.configure('views', {
@@ -11,8 +12,12 @@ nunjucks.configure('views', {
 
 app.set('view engine', 'html');
 app.set('port', process.env.PORT || 3000);
-app.use(router);
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+app.use(router);
 app.listen(app.get('port'), function(){
 	console.log("Server running on " + app.get('port'));
 });
--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+	bodyParser = require('body-parser'),
 	router	= express.Router();
 	
 router.get('/', function(req, res){
@@ -6,18 +7,10 @@ router.get('/', function(req, res){
 });
 router.get('/signup', function(req, res){
 	res.render('signup.html');
+	console.log(req.body.nama);
 });
 router.post('/signup', function(req, res){
-	var data = {
-		nama: req.body.nama,
-		username: req.body.username,
-		email: req.body.email,
-		password: req.body.password,
-		gender: req.body.gender
-	}
-	
-	console.log(data);
-		
+	res.render("result.html", {nama: req.body.nama});
 });
 router.get('/signin', function(req, res){
 	
