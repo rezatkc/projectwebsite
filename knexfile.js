5826b6bd9b6792367efac7b214720b8bd70098f1
andreepratama27 <andreepratama27@gmail.com>
2016-07-01 03:59:37 +0700

    Tambah handler fail

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'latihan'
+    }
+  }
+}
