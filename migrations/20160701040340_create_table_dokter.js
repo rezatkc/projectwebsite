eb1f4edc5a195ebb04267bfee1838a0a69f60902
andreepratama27 <andreepratama27@gmail.com>
2016-07-01 04:07:37 +0700

    Tambah package multer, tambah migrations

---
+++ migrations/20160701040340_create_table_dokter.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+		knex.schema.createTable('dokter', function(table){
+			table.increments('id').primary();
+			table.string('username');
+			table.string('password');
+			table.string('nama');
+			table.string('email');
+      table.string('alamat');
+			table.text('foto');
+		})	
+	])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('dokter')
+  ])
+};
